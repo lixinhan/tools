d0ddd5db71bc51d982802e6ddc0df9779f7eef94
lixinhan <327902941@qq.com>
2017-12-14 16:14:50 +0800

    增加忽略

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file demo.php
- * @author lixinhan <lixinhan@yuanxin-inc.com>
- * @version 2.0
- * @date 2017-12-14
- */
-include  "Tools.php";
-echo $a=\Lixinhan\Tools::getFileSavePath('123.xls','tbk')."\n";
